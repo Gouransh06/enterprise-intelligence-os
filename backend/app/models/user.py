195ab2cb8d3d3215f5ba441545f85dbe0e3675fc
Gouransh06 <gouransh0603@gmail.com>
2026-07-10 21:37:38 +0530

    feat(auth): initialize authentication module

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -9,7 +9,7 @@ from app.database.base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
 
     username: Mapped[str] = mapped_column(
         String(50),
--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -3,13 +3,14 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from app.models import *
+
 
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +21,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from app.models import *
 from app.database.base import Base
+from app.core.config import settings
 
 target_metadata = Base.metadata
 
